542dbda10fefce9f46d15489712b163d919b1291
Eugene Burkov <e.burkov@adguard.com>
2020-11-13 13:46:39 +0300

    querylog: improve test logic and readability

--- internal/querylog/decode_test.go
+++ internal/querylog/decode_test.go
@@ -2,14 +2,11 @@ package querylog
 
 import (
 	"bytes"
-	"encoding/base64"
-	"fmt"
 	"log"
 	"strings"
 	"testing"
 
 	aglog "github.com/AdguardTeam/golibs/log"
-	"github.com/miekg/dns"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -26,78 +23,22 @@ func TestDecode_decodeQueryLog(t *testing.T) {
 
 	aglog.SetLevel(aglog.DEBUG)
 
-	strBytes := &bytes.Buffer{}
-	enc := base64.NewEncoder(base64.StdEncoding, strBytes)
-
 	testCases := []struct {
-		name    string
-		msgFunc func() error
-		want    string
+		name string
+		log  string
+		want string
 	}{{
 		name: "back_compatibility_all_right",
-		msgFunc: func() error {
-			q := &dns.Msg{}
-			q.SetQuestion(dns.Fqdn("google.com"), dns.TypeMX)
-			bts, err := q.Pack()
-			if err != nil {
-				return err
-			}
-			if _, err := enc.Write(bts); err != nil {
-				return err
-			}
-			return nil
-		},
+		log:  `{"Question":"ULgBAAABAAAAAAAAC2FkZ3VhcmR0ZWFtBmdpdGh1YgJpbwAAHAAB","Time":"2020-11-13T12:41:25.970861+03:00"}`,
 		want: "default",
 	}, {
 		name: "back_compatibility_bad_msg",
-		msgFunc: func() error {
-			q := &dns.Msg{}
-			q.SetQuestion(dns.Fqdn(""), dns.TypeMX)
-			bts, err := q.Pack()
-			if err != nil {
-				return err
-			}
-			if _, err := enc.Write(bts); err != nil {
-				return err
-			}
-			return nil
-		},
-		want: "decodeLogEntry err: dns: overflow unpacking uint32\n",
-	}, {
-		name: "back_compatibility_no_questions",
-		msgFunc: func() error {
-			q := &dns.Msg{}
-			q.SetQuestion(dns.Fqdn("google.com"), dns.TypeMX)
-			q.Question = []dns.Question{}
-			bts, err := q.Pack()
-			if err != nil {
-				return err
-			}
-			if _, err := enc.Write(bts); err != nil {
-				return err
-			}
-			return nil
-		},
-		want: "default",
+		log:  `{"Question":"","Time":"2020-11-13T12:41:25.970861+03:00"}`,
+		want: "decodeLogEntry err: dns: overflow unpacking uint16\n",
 	}, {
 		name: "back_compatibility_bad_decoding",
-		msgFunc: func() error {
-			q := &dns.Msg{}
-			q.SetQuestion(dns.Fqdn("google.com"), dns.TypeMX)
-			q.Question = []dns.Question{{Qclass: dns.ClassINET, Qtype: dns.TypeMX}}
-			bts, err := q.Pack()
-			enc := base64.NewEncoder(
-				base64.NewEncoding("################################################################"),
-				strBytes)
-			if err != nil {
-				return err
-			}
-			if _, err := enc.Write(bts); err != nil {
-				return err
-			}
-			return nil
-		},
-		want: "decodeLogEntry err: illegal base64 data at input byte 0\n",
+		log:  `{"Question":"LgBAAABAAAAAAAAC2FkZ3VhcmR0ZWFtBmdpdGh1YgJpbwAAHAAB","Time":"2020-11-13T12:41:25.970861+03:00"}`,
+		want: "decodeLogEntry err: illegal base64 data at input byte 48\n",
 	}}
 
 	for _, tc := range testCases {
@@ -105,18 +46,12 @@ func TestDecode_decodeQueryLog(t *testing.T) {
 			_, err := logOut.Write([]byte("default"))
 			assert.Nil(t, err)
 
-			err = tc.msgFunc()
-			assert.Nil(t, err)
-
-			oldTags := fmt.Sprintf("{\"Question\":%q,\"Time\":\"2006-01-02T15:04:05Z\"}", strBytes)
-
 			l := &logEntry{}
-			decodeLogEntry(l, oldTags)
+			decodeLogEntry(l, tc.log)
 
-			assert.True(t, strings.HasSuffix(logOut.String(), tc.want))
+			assert.True(t, strings.HasSuffix(logOut.String(), tc.want), logOut.String())
 
 			logOut.Reset()
-			strBytes.Reset()
 		})
 	}
 }
